80c4a64bde73d7f0e4c3c09ec3efced918142c58
Stefan Euchehofer <stefan.euchenhofer@gmail.com>
2024-03-16 18:39:40 +0100

    Update framework headings

--- src/partials/frameworks.jsx
+++ src/partials/frameworks.jsx
@@ -11,17 +11,17 @@ const Frameworks = () => {
                     <ul>
                         <li>
                             <a href="https://www.tailwindapp.com" className="text-dark text-decoration-none text-decoration-hover" target="_blank" rel="noopener noreferrer">
-                                <h4>Tailwind</h4>
+                                <h3>Tailwind</h3>
                             </a>
                         </li>
                         <li>
                             <a href="https://getbootstrap.com/docs/5.3/getting-started/introduction/" className="text-dark text-decoration-none text-decoration-hover" target="_blank" rel="noopener noreferrer">
-                                <h4>Bootstrap</h4>
+                                <h3>Bootstrap</h3>
                             </a>
                         </li>
                         <li>
                             <a href="https://semantic-ui.com/" className="text-dark text-decoration-none text-decoration-hover" target="_blank" rel="noopener noreferrer">
-                                <h4>Semantic UI</h4>
+                                <h3>Semantic UI</h3>
                             </a>
                         </li>
                     </ul>
