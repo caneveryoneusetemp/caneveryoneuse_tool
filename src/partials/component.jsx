b395439fd0d882d94982bd4a196c1ddd833f1ec8
Stefan Euchehofer <stefan.euchenhofer@gmail.com>
2024-03-17 17:08:44 +0100

    Refactor component and framework styles

--- src/partials/component.jsx
+++ src/partials/component.jsx
@@ -29,22 +29,20 @@ const Component = (props) => {
     }
 
     return(
-        <div className="col-12 col-lg-4 mb-4">
-            <div className={`component-card ${retrieveBgColor(errorCountTotal)}`}>
-                <div className="component-card-header">{component.name}</div>
-                <div className="component-card-body">
-                    <h5 className="component-card-title">
-                        <b>{errorCountTotal > 0 ? errorCountTotal : 'OK'}</b>
-                        {errorCountTotal > 0 &&
-                            <a href={component.preview_link} target="_blank" rel="noreferrer" aria-label="view errors"><svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 24 24" fill="none" stroke="#fa2c2c" stroke-width="2.5" stroke-linecap="square" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line></svg></a>
-                        }
-                    </h5>
+        <div className={`component-card ${retrieveBgColor(errorCountTotal)}`}>
+            <div className="component-card-header">{component.name}</div>
+            <div className="component-card-body">
+                <h5 className="component-card-title">
+                    <b>{errorCountTotal > 0 ? errorCountTotal : 'OK'}</b>
+                    {errorCountTotal > 0 &&
+                        <a href={component.preview_link} target="_blank" rel="noreferrer" aria-label="view errors"><svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 24 24" fill="none" stroke="#fa2c2c" stroke-width="2.5" stroke-linecap="square" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line></svg></a>
+                    }
+                </h5>
 
-                    <div className="component-card-footer">
-                        {component.preview_link && component.preview_link.length > 0 &&
-                            <a href={component.preview_link} target="_blank" rel="noreferrer" aria-label="open component example"><svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2.5" stroke-linecap="square" stroke-linejoin="round"><g fill="none" fill-rule="evenodd"><path d="M18 14v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h5M15 3h6v6M10 14L20.2 3.8"/></g></svg></a>
-                        }
-                    </div>
+                <div className="component-card-footer">
+                    {component.preview_link && component.preview_link.length > 0 &&
+                        <a href={component.preview_link} target="_blank" rel="noreferrer" aria-label="open component example"><svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2.5" stroke-linecap="square" stroke-linejoin="round"><g fill="none" fill-rule="evenodd"><path d="M18 14v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h5M15 3h6v6M10 14L20.2 3.8"/></g></svg></a>
+                    }
                 </div>
             </div>
         </div>
