ea5abbc03197974818abe74de74972c3e34555b8
Dhanuka Gunarathna <nuwan.dhanuka@gmail.com>
2024-03-17 11:52:27 +0100

    json files updates

--- src/json/index.js
+++ src/json/index.js
@@ -2,7 +2,10 @@ import antdesign from './antdesign.json';
 import jqueryui from './jqueryui.json';
 import materialAngular from './material-angular.json';
 import mui from './mui.json';
+import bootstrap from './bootstrap.json';
+import foundation from './foundation.json';
+import semanticUi from './semantic_ui.json';
 
-const frameworkData = [antdesign, jqueryui, materialAngular, mui];
+const frameworkData = [antdesign, jqueryui, materialAngular, mui, bootstrap, foundation, semanticUi];
 
 export default frameworkData;
