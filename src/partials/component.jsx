109b1d20c7e54a5c5812c737dec9f2f9e0d53d07
Stefan Euchehofer <stefan.euchenhofer@gmail.com>
2024-03-17 16:05:58 +0100

    Refactor component and framework JSX files, update component styles

--- src/partials/component.jsx
+++ src/partials/component.jsx
@@ -4,42 +4,45 @@ const Component = (props) => {
 
     const { component } = props;
 
-    const calculateErrors = ( component ) => {
+    const calculateErrors = (component) => {
 
         let count = 0;
 
-        if ( ! component ) return count;
+        if (!component) return count;
 
-        if ( component.Axe && component.Axe.total_issues ) {
+        if (component.Axe && component.Axe.total_issues) {
             count = count + parseInt( component.Axe.total_issues, 10 );
         }
 
-        if ( component.manual_testing && component.manual_testing.total_issues ) {
-            count = count + parseInt( component.manual_testing.total_issues, 10 );
+        if (component.manual_testing && component.manual_testing.total_issues) {
+            count = count + parseInt(component.manual_testing.total_issues, 10);
         }
 
         return count;
     }
 
-    const errorsCount = calculateErrors( component );
+    const errorCountTotal = calculateErrors(component);
+
+    const retrieveBgColor = (count) => {
+        if (count > 3) return 'bg-danger';
+        return count > 0 ? 'bg-warning' : 'bg-success';
+    }
 
     return(
         <div className="col-12 col-lg-4 mb-4">
-            <div className="card">
-                <div className="card-header">{component.name}</div>
-                <div className="card-body">
-                    <h5 className="card-title d-flex align-items-center">
-                        <span>Found accessibility errors:</span>
-                        <span className={`py-2 px-3 text-white bg-${errorsCount === 0 ? 'success' : 'warning'} ms-2`}>{errorsCount}</span>
+            <div className={`component-card ${retrieveBgColor(errorCountTotal)}`}>
+                <div className="component-card-header">{component.name}</div>
+                <div className="component-card-body">
+                    <h5 className="component-card-title">
+                        <b>{errorCountTotal > 0 ? errorCountTotal : 'OK'}</b>
+                        {errorCountTotal > 0 &&
+                            <a href={component.preview_link} target="_blank" rel="noreferrer"><svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 24 24" fill="none" stroke="#fa2c2c" stroke-width="2.5" stroke-linecap="square" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="12"></line><line x1="12" y1="16" x2="12.01" y2="16"></line></svg></a>
+                        }
                     </h5>
 
-                    <div className="card-footer">
-                        {  component.preview_link && component.preview_link.length > 0 &&
-                            <a href={component.preview_link} className="btn btn-primary" target="_blank" rel="noreferrer">Preview</a>
-                        }
-
-                        {errorsCount > 0 &&
-                            <button type="button" className="btn btn-secondary ms-2">Check errors</button>
+                    <div className="component-card-footer">
+                        {component.preview_link && component.preview_link.length > 0 &&
+                            <a href={component.preview_link} target="_blank" rel="noreferrer"><svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2.5" stroke-linecap="square" stroke-linejoin="round"><g fill="none" fill-rule="evenodd"><path d="M18 14v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h5M15 3h6v6M10 14L20.2 3.8"/></g></svg></a>
                         }
                     </div>
                 </div>
--- src/partials/single-framework.jsx
+++ src/partials/single-framework.jsx
@@ -11,7 +11,7 @@ const SingleFramework = (props) => {
             <div className="row">
                 <div className="col-12 my-5">
                     <div className="mb-4">
-                        <Link to="/">Go back</Link>
+                        <Link to="/"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"><path d="M19 12H6M12 5l-7 7 7 7"/></svg> Back</Link>
                     </div>
                     <h2>{framework.name}</h2>
                 </div>
