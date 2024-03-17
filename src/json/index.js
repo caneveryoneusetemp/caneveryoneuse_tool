86946c983c05e25743467485546c3c0c025816ec
Thomas Stauer <thomas.stauer@4fb.de>
2024-03-17 12:49:43 +0100

    align "jquery ui" json filename name with "semantic ui" and "tailwind ui"

--- src/json/index.js
+++ src/json/index.js
@@ -1,11 +1,11 @@
 import antdesign from './antdesign.json';
-import jqueryui from './jqueryui.json';
+import jqueryUi from './jquery_ui.json';
 import materialAngular from './material-angular.json';
 import mui from './mui.json';
 import bootstrap from './bootstrap.json';
 import foundation from './foundation.json';
 import semanticUi from './semantic_ui.json';
 
-const frameworkData = [antdesign, jqueryui, materialAngular, mui, bootstrap, foundation, semanticUi];
+const frameworkData = [antdesign, jqueryUi, materialAngular, mui, bootstrap, foundation, semanticUi];
 
 export default frameworkData;
