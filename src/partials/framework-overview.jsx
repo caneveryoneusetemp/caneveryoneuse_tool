d485d8a5bff4a1de3f1f2e6d038750863bf639d6
AndiR16 <andir16@gmx.ch>
2024-03-18 10:09:55 +0100

    Adds link to issues in overview table

--- src/partials/framework-overview.jsx
+++ src/partials/framework-overview.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import frameworkData from '../json';
 import { Link } from "react-router-dom";
 
-
 const calculateTotalIssues = (component) => {
   let totalIssues = 0;
 
@@ -27,27 +26,32 @@ const getBackgroundClass = (issues) => {
 
 const FrameworkOverview = () => {
   const allComponentNames = [...new Set(frameworkData.flatMap(framework =>
-    framework.components.map(component => component.name)))];
+    framework.components.map(component => ({ name: component.name, slug: component.slug }))))];
 
-  const componentIssuesMap = allComponentNames.reduce((acc, name) => {
+  const componentIssuesMap = allComponentNames.reduce((acc, { name, slug }) => {
     acc[name] = frameworkData.map(framework => {
       const component = framework.components.find(c => c.name === name);
-      return component ? calculateTotalIssues(component) : '-';
+      return component ? { issues: calculateTotalIssues(component), slug: `${framework.slug}/${component.slug}` } : { issues: '-', slug: null };
     });
     return acc;
   }, {});
 
-  // Inline style for equal column width
+  const linkStyle = {
+    textDecoration: 'none', 
+    color: 'black', 
+  };
+
   const columnStyle = {
     width: `${100 / (frameworkData.length + 1)}%`,
     textAlign: 'center',
   };
 
+
   return (
     <div className="container">
       <div className="mb-4">
-        <Link to={`/`}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="square" stroke-linejoin="round"><path d="M19 12H6M12 5l-7 7 7 7"/></svg> Back
+        <Link to={`/`} style={linkStyle}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M19 12H5M12 19l-7-7 7-7"/></svg> Back
         </Link>
       </div>
       <h2>Frameworks Component Issues Overview</h2>
@@ -64,8 +68,12 @@ const FrameworkOverview = () => {
           {Object.entries(componentIssuesMap).map(([componentName, issuesCounts], index) => (
             <tr key={index}>
               <td style={columnStyle}>{componentName}</td>
-              {issuesCounts.map((count, fwIndex) => (
-                <td key={fwIndex} className={`${getBackgroundClass(count)} text-center text-bkack`} style={columnStyle}>{count}</td>
+              {issuesCounts.map(({ issues, slug }, fwIndex) => (
+                <td key={fwIndex} className={`${getBackgroundClass(issues)} text-center`} style={columnStyle}>
+                  {issues !== '-' && parseInt(issues, 10) > 0 ? (
+                    <Link to={`/${slug}`} style={linkStyle}>{issues}</Link>
+                  ) : issues}
+                </td>
               ))}
             </tr>
           ))}
