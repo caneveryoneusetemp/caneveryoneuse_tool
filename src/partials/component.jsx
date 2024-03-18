fe57b0f6f744b91108e3c16a59ae544d9e95aae3
Thomas Stauer <thomas.stauer@4fb.de>
2024-03-18 10:42:44 +0100

    do not read the image for the goto errors link as it's purely decorative

--- src/partials/component.jsx
+++ src/partials/component.jsx
@@ -100,7 +100,7 @@ const Component = (props) => {
 
                         <div className="component-card-footer d-flex flex-column text-center">
                             {selectedComponent.preview_link && selectedComponent.preview_link.length > 0 &&
-                                <a href={selectedComponent.preview_link} target="_blank" rel="noreferrer" aria-label="open component example"><svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 24 24" fill="none" stroke="#000" strokeWidth="2.5" strokeLinecap="square" strokeLinejoin="round"><g fill="none" fillRule="evenodd"><path d="M18 14v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h5M15 3h6v6M10 14L20.2 3.8"/></g></svg></a>
+                                <a href={selectedComponent.preview_link} target="_blank" rel="noreferrer" aria-label="open component example"><svg aria-hidden={true} xmlns="http://www.w3.org/2000/svg" width="33" height="33" viewBox="0 0 24 24" fill="none" stroke="#000" strokeWidth="2.5" strokeLinecap="square" strokeLinejoin="round"><g fill="none" fillRule="evenodd"><path d="M18 14v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h5M15 3h6v6M10 14L20.2 3.8"/></g></svg></a>
                             }
                         </div>
                     </div>
