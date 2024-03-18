c123b8b131f4e0a7015058fdc16aadcba8505a03
Thomas Stauer <thomas.stauer@4fb.de>
2024-03-18 10:36:03 +0100

    hide decorative back image to improve accessibility with screen readers

--- src/partials/framework-overview.jsx
+++ src/partials/framework-overview.jsx
@@ -50,7 +50,7 @@ const FrameworkOverview = () => {
     <div className="container">
       <div className="mb-4">
         <Link to={`/`}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000" strokeWidth="2" strokeLinecap="square" strokeLinejoin="round"><path d="M19 12H6M12 5l-7 7 7 7"/></svg> Back
+            <svg aria-hidden={true} xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000" strokeWidth="2" strokeLinecap="square" strokeLinejoin="round"><path d="M19 12H6M12 5l-7 7 7 7"/></svg> Back
         </Link>
       </div>
       <h2>Frameworks Component Issues Overview</h2>
