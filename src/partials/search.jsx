ff37726d501781cb6aee88c9a9e37f9fd54589f8
Stefan Euchehofer <stefan.euchenhofer@gmail.com>
2024-03-18 09:16:04 +0100

    Refactor search component styles

--- src/partials/search.jsx
+++ src/partials/search.jsx
@@ -13,7 +13,6 @@ const Search = () => {
             margin: '0 auto',
             minHeight: '4rem',
             padding: '0 2rem',
-            boxShadow: '5px 5px rgba(73, 80, 87, 0.2)',
         }
     }
 
@@ -25,7 +24,7 @@ const Search = () => {
                     <div className="col-12">      
                         <form class="d-flex flex-column text-center">
                             <label htmlFor="search_q" className="form-label h3 mb-4"><h1 style={style.h1} className="mb-2">Search Component or Framework</h1>to check it's accessibility score.</label>
-                            <input style={style.input} type="text" className="form-control border-dark rounded-4 bg-white" id="search_q" placeholder="Search here..." value={searchq} onChange={(e) => setSearchq(e.target.value)} />
+                            <input style={style.input} type="text" className="form-control form-control-search rounded-4 bg-white" id="search_q" placeholder="Search here..." value={searchq} onChange={(e) => setSearchq(e.target.value)} />
                         </form>
                     </div>
                 </div>
