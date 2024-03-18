f1de72f3d994ab7f381cdab90951ac7b86d1a6f9
Stefan Euchehofer <stefan.euchenhofer@gmail.com>
2024-03-18 10:28:21 +0100

    Update footer content and layout

--- src/partials/footer.jsx
+++ src/partials/footer.jsx
@@ -4,7 +4,15 @@ const Footer = () => {
     return(
         <div className='bg-black mt-5'>
             <div className='container mt-5 py-3 pt-5 text-white'>
-                Because Accessibility matters<b className='text-brand'>.</b>
+                <div className='row'>
+                    <div class="col">
+                        We are currently aware that our website may not be fully accessible to all users. This site serves as a minimum viable product (MVP) developed for a hackathon and is still in its early stages of development.
+                    </div>
+                    <div class="col text-end">
+                        Proudly developed at the <a className='text-white' href='https://hackathon.cloudfest.com/' target="_blank" rel="noreferrer">CloudFest Hackathon 2024</a>. 
+                        <br />❤️ Because Accessibility matters.
+                    </div>
+                </div>
             </div>
         </div>
     );
