b38ae16a0981a52c97efd1157f891d3ace2b96cc
Thomas Stauer <thomas.stauer@4fb.de>
2024-03-18 10:43:51 +0100

    fix warning about property class not being defined

--- src/partials/footer.jsx
+++ src/partials/footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
         <div className='bg-black mt-5'>
             <div className='container mt-5 py-3 pt-5 text-white'>
                 <div className='row'>
-                    <div class="col">
+                    <div className="col">
                         We are currently aware that our website may not be fully accessible to all users. This site serves as a minimum viable product (MVP) developed for a hackathon and is still in its early stages of development.
                     </div>
-                    <div class="col text-end">
+                    <div className="col text-end">
                         Proudly developed at the <a className='text-white' href='https://hackathon.cloudfest.com/' target="_blank" rel="noreferrer">CloudFest Hackathon 2024</a>. 
                         <br />❤️ Because Accessibility matters.
                     </div>
